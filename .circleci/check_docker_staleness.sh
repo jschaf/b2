e0cd0c9c8fb631cf712eaf9e4c4e01f54c0cef49
Joe Schafer <joe@jschaf.com>
2019-10-03 03:51:00 -0700

    Add docker build step

---
+++ .circleci/check_docker_staleness.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Builds the Docker image for this project.
+#
+# If none of the dependencies that build the Docker image changed, this script
+# halts the CircleCI job that runs this script.
+
+printf '
+# Checking if Docker image is stale
+===================================\n'
+
+# The hash file persisted in the workspace by the checkout_code job.
+current_hash_file='.circleci/docker_cache_key'
+if [[ ! -e "${current_hash_file}" ]]; then
+  echo "ERROR: cache key file not found at ${current_hash_file}."
+  exit 1
+fi
+current_hash="$(< "${current_hash_file}")"
+
+# The hash file of the already built docker file.
+precheck_hash_file=".circleci/docker_precheck_cache_key"
+precheck_hash='<none>'
+precheck_status='stale'
+if [[ -f "${precheck_hash_file}" ]]; then
+  precheck_hash="$(< ${precheck_hash_file})"
+  if [[ "${current_hash}" == "${precheck_hash}" ]]; then
+    precheck_status='fresh'
+  fi
+fi
+
+printf 'Hashes in %-65s\n%s\n\n' "${current_hash_file}" "${current_hash}"
+printf 'Hashes in %-65s\n%s\n\n' "${precheck_hash_file}" "${precheck_hash}"
+echo "cache_status: ${precheck_status}"
+
+if [[ "${precheck_status}" == 'fresh' ]]; then
+  printf '\nSkipping building Docker image because because the cache is fresh.\n'
+  circleci task halt
+  exit 0
+fi
+
+printf '
+# Installing Docker image because it is stale
+=============================================\n'
+
+make docker-image
---
+++ .circleci/create_docker_cache_key.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euo pipefail
+
+# Creates a combined cache key to use for the CircleCI docker cache.
+#
+# A clean build of the Docker image depends on more than a single file. CircleCI
+# can't use multiple files as a cache key, so store the hashes of all
+# dependencies in a single file that we can then reference from a save_cache
+# step. See https://discuss.circleci.com/t/base-cache-key-on-checksum-of-directory-rather-than-a-single-file/20059/4.
+
+# The hashes of all dependencies for building the Docker image.
+create_cache_key_script_hash="$(sha1sum .circleci/create_docker_cache_key.sh)"
+makefile_hash="$(sha1sum Makefile)"
+dockerfile_hashes="$(find docker/blog -type f -print0 | xargs -0 sha1sum | sort)"
+
+# Where to save the hashes.
+cache_key_file='.circleci/docker_cache_key'
+
+# If you change the logic for either building the Docker image, update
+# this key to the current time. It's easier to change it here than changing
+# the cache name in .circleci/config.yml.
+cache_bust_key='2019-10-01T19:49'
+
+printf '\nWriting current Docker cache key to %s.\n\n' ${cache_key_file}
+echo "# Combined cache key hashes
+cache_buster=${cache_bust_key}
+${create_cache_key_script_hash}
+${makefile_hash}
+${dockerfile_hashes}" > ${cache_key_file}
+
+cat "${cache_key_file}"
