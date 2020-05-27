a92a29bf002c24350def74ed895d377b4f9e60c0
Joe Schafer <joe@jschaf.com>
2020-05-27 11:25:01 -0700

    Remove citation tests

--- pkg/markdown/mdext/citation_test.go
+++ pkg/markdown/mdext/citation_test.go
@@ -8,9 +8,7 @@ func TestNewCitationExt(t *testing.T) {
 	tests := []struct {
 		src  string
 		want string
-	}{
-		{"qux [foo] bar\n\n[foo]: http://example.com", "<p>foo</p>"},
-	}
+	}{}
 
 	for _, tt := range tests {
 		t.Run(tt.src, func(t *testing.T) {
