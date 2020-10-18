728e7a6b005b2722bb90b9a3d01a808572010323
Joe Schafer <joe@jschaf.com>
2020-10-17 21:03:08 -0700

    Add chans.Wait

---
+++ pkg/chans/chans.go
@@ -0,0 +1,21 @@
+package chans
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	TimeoutErr = errors.New("timeout waiting for channel")
+)
+
+// Wait waits for ch to close for up to duration d. If ch doesn't close with
+// duration d, Wait returns a TimeoutError.
+func Wait(ch <-chan struct{}, d time.Duration) error {
+	select {
+	case <-ch:
+		return nil
+	case <-time.After(d):
+		return TimeoutErr
+	}
+}
