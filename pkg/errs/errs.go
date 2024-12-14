11e14cb5821c079fc1f97cc17bf6cee9071e0182
Joe Schafer <joe.schafer@delta46.us>
2024-12-14 14:43:48 -0500

    Simplify errs.CaptureT

--- pkg/errs/errs.go
+++ pkg/errs/errs.go
@@ -13,37 +13,24 @@ import (
 //   - If errF returns an error and *err == nil, replace *err with the error.
 //   - If errF returns an error and *err != nil, replace *err with a errors.Join
 //     containing *err and the errF err.
-func Capture(err *error, errF func() error, msg string) {
-	fErr := errF()
-	if fErr == nil {
+func Capture(errPtr *error, errFunc func() error, msg string) {
+	err := errFunc()
+	if err == nil {
 		return
 	}
-	wErr := fErr
-	if msg != "" {
-		wErr = fmt.Errorf(msg+": %w", fErr)
-	}
-	if *err == nil {
-		// Only 1 error so avoid a MultiError and replace the err pointer.
-		*err = wErr
-		return
-	}
-	*err = errors.Join(*err, wErr)
+	*errPtr = errors.Join(*errPtr, fmt.Errorf("%s: %w", msg, err))
 }
 
+// testingTB is a subset of *testing.T and *testing.B methods.
 type testingTB interface {
 	Helper()
-	Error(args ...interface{})
 	Errorf(format string, args ...interface{})
 }
 
 // CaptureT call t.Error if errF returns an error with an optional message.
-func CaptureT(t testingTB, errF func() error, msg string) {
+func CaptureT(t testingTB, errFunc func() error, msg string) {
 	t.Helper()
-	if err := errF(); err != nil {
-		if msg == "" {
-			t.Error(err)
-		} else {
-			t.Errorf(msg+": %s", err)
-		}
+	if err := errFunc(); err != nil {
+		t.Errorf("%s: %s", msg, err)
 	}
 }
